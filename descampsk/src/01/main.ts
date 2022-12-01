540a90e8367bb1210e7ef02efad6911ff76dd642
descampsk <kevin.descamps@tinyclues.com>
2022-12-01 09:36:08 +0100

    [descampsk] day 01

--- descampsk/src/01/main.ts
+++ descampsk/src/01/main.ts
@@ -1,14 +1,59 @@
 import { readInputs } from "../helpers/read-inputs";
 
-const TEST_INPUT_PATH = `${__dirname}/input.test`;
-const INPUT_PATH = `${__dirname}/input.test`;
+const TEST_INPUT_PATH = `${__dirname}/input.test.txt`;
+const INPUT_PATH = `${__dirname}/input.txt`;
 
 const resolveFirstPuzzle = async (inputPath: string) => {
   const lines = await readInputs(inputPath);
+  let max = 0;
+  let currentSum = 0;
+  lines.forEach((line, index) => {
+    const calory = Number.parseInt(line, 10);
+    if (Number.isNaN(calory)) {
+      console.log(max, currentSum, index);
+      if (currentSum > max) {
+        max = currentSum;
+        currentSum = 0;
+      } else {
+        currentSum = 0;
+      }
+    } else {
+      currentSum += calory;
+    }
+  });
+  return max;
 };
 
 const resolveSecondPuzzle = async (inputPath: string) => {
   const lines = await readInputs(inputPath);
+  let top1 = 0;
+  let top2 = 0;
+  let top3 = 0;
+  let currentSum = 0;
+  lines.forEach((line, index) => {
+    const calory = Number.parseInt(line, 10);
+    if (Number.isNaN(calory)) {
+      if (currentSum > top1) {
+        top3 = top2;
+        top2 = top1;
+        top1 = currentSum;
+        currentSum = 0;
+      } else if (currentSum > top2) {
+        top3 = top2;
+        top2 = currentSum;
+        currentSum = 0;
+      } else if (currentSum > top3) {
+        top3 = currentSum;
+        currentSum = 0;
+      } else {
+        currentSum = 0;
+      }
+      console.log(top1, top2, top3, currentSum, index);
+    } else {
+      currentSum += calory;
+    }
+  });
+  return top1 + top2 + top3;
 };
 
 const main = async () => {
