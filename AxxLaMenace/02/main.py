67d216e81cf2e6f4a912531369f1ffd3bdf08e19
Axel-IO <axel.joigneau@innovorder.fr>
2022-12-02 08:18:32 +0100

    si on joue le puits ça marche ?

---
+++ AxxLaMenace/02/main.py
@@ -0,0 +1,30 @@
+import os
+
+RPC1 = 'ABC'
+RPC2 = 'XYZ'
+
+def solve_first_puzzle(data):
+    score = 0
+    for move1, move2 in data:
+        a = RPC1.index(move1)
+        b = RPC2.index(move2)
+        win_lose = 6 if b==(a+1)%3 else 3 if a==b else 0
+        score += win_lose + b+1
+    return score
+
+
+def solve_second_puzzle(data):
+    score = 0
+    for move1, move2 in data:
+        a = RPC1.index(move1)
+        b = RPC2.index(move2)
+        b = (a+b-1)%3
+        win_lose = 6 if b==(a+1)%3 else 3 if a==b else 0
+        score += win_lose + b+1
+    return score
+
+if __name__ == '__main__':
+    with open(os.path.join(os.path.dirname(__file__), 'data.txt')) as f:
+        data = [line.split(' ') for line in f.read().splitlines()]
+    print("result first puzzle:", solve_first_puzzle(data))
+    print("result second puzzle:", solve_second_puzzle(data))
